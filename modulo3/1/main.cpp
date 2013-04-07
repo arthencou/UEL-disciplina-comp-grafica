06f3014b3917c83be44ae7b2b5be7ce432170e6b
Breno Naodi Kusunoki <brenokusunoki@gmail.com>
2013-04-06 22:55:44 -0300

    Adicionado o exercício 2.

--- modulo3/1/main.cpp
+++ modulo3/1/main.cpp
@@ -1,69 +1,78 @@
+/*
+ * Módulo 3 - Exercício 1
+ * Nomes:
+ *      Arthur Henrique Coutinho
+ *      Breno Naodi Kusunoki
+ *      Luiz Guilherme Castilho Martins
+ */
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
 
-//foi utilizado o exemplo do livro texto da disciplina:
-//         Computer Graphics using Open GL, 2nd edition, by Francis S. Hill
-//inserir no linker da opção de projetos: -lopengl32 -lglut32 -lglu32
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#include <math.h>
+#include <stdio.h>
 
-#include "glut.h"
+using namespace std;
 
-#include <math.h> 
-
-// coordenadas mundo
-void setWindow(float left,float right,float bottom,float top)
+void myInit(void)
 {
-glMatrixMode(GL_PROJECTION);
-glLoadIdentity();
-gluOrtho2D(left,right,bottom,top);
-}
-// subjanela de visualização
-// considera a janela definida no comando glutInitWindowSize(640,480); 
-//                                        *** tamanho da Janela ***
-void setViewport(int left,int right,int bottom,int top)
-{
-glViewport(left,bottom,right-left,top-bottom);
+        glClearColor(1.0,1.0,1.0,0.0);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glPointSize(4.0);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+        glLineWidth(3.4f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glFrontFace(GL_CW);
+        glColor3f(0.0f, 0.0f, 0.0f);
+
 }
 
- void myInit(void)
- {
-    glClearColor(1.0,1.0,1.0,0.0);        // set white background color
-    glColor3f(0.0f, 0.0f, 0.0f);          // set the drawing color
-    glPointSize(4.0);                 // a 'dot' is 4 by 4 pixels
-    glLineWidth(2.0);       
-}
-//<<<<<<<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>
-void plotsin()
+void padrao(int x, int y)
 {
-    glBegin(GL_LINE_STRIP); {
-	for(GLfloat x = -4.0; x < 4.0; x += 0.1)
-	    {
-		GLfloat y = sin(3.14159 * x) / (3.14159 * x);
-		glVertex2f(x, y);
-	    }		
-    } glEnd();
-    
-    glFlush();   
+              glBegin(GL_LINE_STRIP);
+                glVertex2i( x, y);
+                glVertex2i( x, y + 40);
+                glVertex2i( x +  40, y + 60);
+                glVertex2i( x +  40, y + 100);
+                glVertex2i( x +  80, y + 80);
+                glVertex2i( x +  80, y + 40);
+                glVertex2i( x + 120, y + 20);
+                glVertex2i( x +  80, y);
+                glVertex2i( x +  40, y + 20);
+                glVertex2i(x, y);
+        glEnd();
+        glFlush();
 }
+
 void myDisplay(void)
-{ 
-     setWindow(-5.0,5.0,-0.3,1.0); // coordenada mundo obs:pode-se inverter os eixos para que a figura fique de cabeça para baixo
-     setViewport(0,640,0,480);     // Sub Janela de visualização  
-     glClear(GL_COLOR_BUFFER_BIT);
-           
-     setViewport(0,640,0,480);
-     plotsin();
-      
-     setViewport(340,640,260,480);
-     plotsin();
+{
+        padrao(100,100);
+        padrao(220, 120);
+        padrao(340, 140);
+        padrao(140, 200);
+        padrao(260, 220);
+        padrao(180, 20);
+        padrao(300, 40);
+        padrao(340, 340);
 }
-//<<<<<<<<<<<<<<<<<<<<<<<< main >>>>>>>>>>>>>>>>>>>>>>
+
 int main(int argc, char** argv)
 {
-        glutInit(&argc, argv);            // initialize the toolkit
-        glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // set display mode
-        glutInitWindowSize(640,480);          // *** tamanho da Janela ***
-        glutInitWindowPosition(100, 150);     // set window position on screen
-        glutCreateWindow("Primeiro programa"); // open the screen window
-        glutDisplayFunc(myDisplay);           // register redraw function
+        glutInit(&argc, argv);
+        glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+        glutInitWindowSize(640,480);
+        glutInitWindowPosition(100, 150);
+        glutCreateWindow("Primeiro programa");
+        glutDisplayFunc(myDisplay);
         myInit();
-        glutMainLoop();                       // go into a perpetual loop
+        glutMainLoop();
 }
+
 
